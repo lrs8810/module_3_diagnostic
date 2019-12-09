8297fdfb412c5326abdc085a0bc07c2c04c994cf
Laura Schulz <lrs8810@gmail.com>
2019-12-09 10:58:51 -0700

    Update index action with locals

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,21 +1,7 @@
 class SearchController < ApplicationController
   def index
-    def connection
-      Faraday.new("https://www.potterapi.com/v1/") do |faraday|
-        faraday.params['key'] = ENV['API_KEY']
-        faraday.adapter Faraday.default_adapter
-      end
-    end
-
-    def get_json(house)
-      raw_data = connection.get('characters') do |chars|
-        chars.params['house'] = house
-        chars.params['orderOfThePhoenix'] = true
-      end
-      json_data = JSON.parse(raw_data.body)
-      json_data.count
-    end
-
-    @house_results = get_json('Gryffindor')
+    render locals: {
+      house_results: HouseResults.new(params[:house])
+    }
   end
 end
